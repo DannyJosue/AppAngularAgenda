92df2a336529eb5cc1070b51a7e091f70e5ce530
DANNY\Danny <dannyjosue89@gmail.com>
2017-07-20 20:13:18 -0600

    Creacion de vistas y  funciones de CRUD.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# AppAngularAgenda
-Aplicacion cliente Agenda
+# TutorialIn6av
+
+## Instalacion
+
+1. `cd TutorialIn6av`.
+2. `npm install`.
+
+## Ejecucion
+
+Ejecutar el siguiente comando `ng serve -o` y automaticamente desplegará el navegador con la aplicación corriendo.
---
+++ src/app/components/dashboard/contacto/contacto.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ContactoService } from '../../../services/contacto.service';
+
+@Component({
+  selector: 'app-contacto',
+  templateUrl: 'contacto.component.html',
+  providers: [ContactoService]
+})
+export class ContactoComponent implements OnInit {
+  constructor(private contactoService: ContactoService) {  }
+  url:string;
+    formularioContacto:FormGroup;
+    formularioUpdate:FormGroup;
+
+ ngOnInit() {
+    let validaciones = [
+      Validators.required, Validators.minLength(2)
+    ]
+
+    this.contactoService.getContactos().subscribe();
+
+    this.formularioContacto = new FormGroup({
+      'idUsuario': new FormControl('', validaciones),
+      'nombre': new FormControl('', validaciones),
+      'apellido': new FormControl('', validaciones),
+      'telefono': new FormControl('', validaciones),
+      'direccion': new FormControl('', validaciones),
+      'idCategoria': new FormControl('', validaciones),
+    });
+  }
+
+  private actualizarPage() {
+    this.contactoService.getContactos().subscribe();
+    this.ngOnInit();
+  }
+  
+  public agregarContacto() {
+    console.log(this.formularioContacto.value);
+    this.contactoService.agregarContacto(this.formularioContacto.value)
+    this.actualizarPage();
+  }
+
+
+  public eliminar(idContacto:number) {
+  this.contactoService.eliminarContacto(idContacto)
+    .subscribe(res => {
+      if(res.estado) {
+        console.log(idContacto);
+        this.actualizarPage();
+      }
+    })    
+  } 
+   
+  
+}
